8ec3740f6e6f06746c097b8b66d03d64a67020d1
Kellen Donohue <kellend@cs.washington.edu>
2012-12-05 03:09:02 -0800

    Add start of WPF unit test. It's not all named properly yet.

---
+++ DotNetFrontEndTests/WpfBasic/WpfBasic/App.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Windows;
+
+namespace WpfBasic
+{
+  /// <summary>
+  /// Interaction logic for App.xaml
+  /// </summary>
+  public partial class App : Application
+  {
+    public static WpfBasicModel model;
+    public App()
+    {
+      model = new WpfBasicModel();
+    }
+  }
+}
