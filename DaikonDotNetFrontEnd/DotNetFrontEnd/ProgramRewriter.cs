bcf86e22d75b5e4f454201ff400f18d19200bd5f
Kellen Donohue <kellend@cs.washington.edu>
2013-03-29 13:39:03 -0700

    Refactor program rewriter with helper methods instead of just one big method. Also add suppressions for the program rewriter console writeline warnings.

---
+++ DaikonDotNetFrontEnd/DotNetFrontEnd/ProgramRewriter.cs
@@ -0,0 +1,251 @@
+﻿//-----------------------------------------------------------------------------
+//
+// Copyright (c) Microsoft, Kellen Donohue. All rights reserved.
+// This code is licensed under the Microsoft Public License.
+// THIS CODE IS PROVIDED *AS IS* WITHOUT WARRANTY OF
+// ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING ANY
+// IMPLIED WARRANTIES OF FITNESS FOR A PARTICULAR
+// PURPOSE, MERCHANTABILITY, OR NON-INFRINGEMENT.
+//
+// This file defines a profiler that inserts instrumentation calls 
+// at the beginning and end of every function and returns the modified program
+// as a MemoryStream.
+//
+//-----------------------------------------------------------------------------
+
+// This file and ILRewriter.cs originally came from the CCIMetadata (http://ccimetadata.codeplex.com/) 
+// sample programs. Specifically, it was Samples/ILMutator/ILMutator.cs. The original code inserted
+// a System.WriteLine call for each store to a local variable defined by the programmer. It has been
+// modified to insert the call to the instrumentation function at the beginning and exit of every 
+// method. This is mostly done in the ProcessOperations() method, with many support methods added.
+
+using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using DotNetFrontEnd.Comparability;
+using Microsoft.Cci;
+using Microsoft.Cci.ILToCodeModel;
+using Microsoft.Cci.MutableCodeModel;
+
+namespace DotNetFrontEnd
+{
+  /// <summary>
+  /// Performs insertion of instrumentation calls at entrance and exit of every function in a
+  /// executable. Never instantiated, only the RewriteProgramIL() method is used.
+  /// </summary>
+  public static class ProgramRewriter
+  {
+    #region Constants
+
+    /// <summary>
+    /// The environment variable defining where front-end output will go
+    /// </summary>
+    private static readonly string DaikonEnvVar = "DNFE_OUT";
+
+    /// <summary>
+    /// The name of the DLL containing the visitor to insert calls to
+    /// </summary>
+    public static readonly string VisitorDll = "DotNetFrontEnd.dll";
+
+    #endregion
+
+    /// <summary>
+    /// Rewrite program IL with instrumentation calls, returning a MemoryStream, or null if the
+    /// program was saved to a file.
+    /// Caller must be sure to dispose returned MemoryStream.
+    /// </summary>
+    /// <param name="frontEndArgs">The args to use in profiling the program</param>
+    /// <returns>MemoryStream containing the program to be profiled, with the instrumentation
+    /// code added, or null if the saveProgram argument is active.</returns>
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage",
+       "CA2202:Do not dispose objects multiple times"),
+     System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Reliability",
+       "CA2000:Dispose objects before losing scope")]
+    public static MemoryStream RewriteProgramIL(FrontEndArgs frontEndArgs, TypeManager typeManager)
+    {
+      if (String.IsNullOrWhiteSpace(frontEndArgs.AssemblyPath))
+      {
+        throw new FileNotFoundException("Path to program to be profiled not provided");
+      }
+
+      Stream resultStream;
+      var host = typeManager.Host;
+
+      IModule/*?*/ module = host.LoadUnitFrom(frontEndArgs.AssemblyPath) as IModule;
+      if (module == null || module == Dummy.Module || module == Dummy.Assembly)
+      {
+        throw new FileNotFoundException("Given path is not a PE file containing a CLR"
+          + " assembly, or an error occurred when loading it.", frontEndArgs.AssemblyPath);
+      }
+
+      PdbReader pdbReader;
+      string pdbFile;
+      LoadPdbReaderAndFile(frontEndArgs, typeManager, module, out pdbReader, out pdbFile);
+
+      Assembly mutable = null;
+
+      using (pdbReader)
+      {
+        AssemblySummary comparabilityManager = GenerateComparability(frontEndArgs, typeManager, 
+          host, module, pdbReader, ref mutable);
+
+        if (frontEndArgs.GenerateComparability && frontEndArgs.ComparabilityFile == null)
+        {
+          return null;
+        }
+
+        ILRewriter mutator = new ILRewriter(host, pdbReader, frontEndArgs, typeManager, comparabilityManager);
+
+        module = mutator.Visit(mutable, Path.Combine(FindVisitorDir(), VisitorDll));
+
+        if (frontEndArgs.EmitNullaryInfo || frontEndArgs.GenerateComparability)
+        {
+          return null;
+        }
+
+        // Remove the old PDB file
+        try
+        {
+          File.Delete(pdbFile);
+        }
+        catch (UnauthorizedAccessException)
+        {
+          // If they are running the debugger we might not be able to delete the file
+          // Save the pdb elsewhere in this case.
+          pdbFile = module.Location + ".pdb";
+        }
+
+        if (frontEndArgs.SaveProgram != null)
+        {
+          resultStream = new FileStream(frontEndArgs.SaveProgram, FileMode.Create);
+        }
+        else
+        {
+          resultStream = new MemoryStream();
+        }
+
+        // Need to not pass in a local scope provider until such time as we have one 
+        // that will use the mutator to remap things (like the type of a scope 
+        // constant) from the original assembly to the mutated one.
+        using (var pdbWriter = new PdbWriter(pdbFile, pdbReader))
+        {
+          PeWriter.WritePeToStream(module, host, resultStream, pdbReader, null, pdbWriter);
+        }
+      }
+
+      if (frontEndArgs.SaveProgram != null)
+      {
+        // We aren't going to run the program, so no need to return anything,
+        // but close the file stream.
+        resultStream.Close();
+        return null;
+      }
+      else
+      {
+        return (MemoryStream)resultStream; // success
+      }
+    }
+
+    /// <summary>
+    /// Find the directory the Celeriac DLL is located in, based on envrionmental variables.
+    /// </summary>
+    /// <returns>Absolute path to the celeriac directory.</returns>
+    private static string FindVisitorDir()
+    {
+      // Look for the path to the reflector, it's an environment variable, check the user space 
+      // first.
+      string daikonDir = Environment.GetEnvironmentVariable(DaikonEnvVar,
+          EnvironmentVariableTarget.User);
+      if (daikonDir == null)
+      {
+        // If that didn't work check the machine space
+        daikonDir = Environment.GetEnvironmentVariable(DaikonEnvVar,
+            EnvironmentVariableTarget.Machine);
+      }
+
+      if (daikonDir == null)
+      {
+        // We can't proceed without this
+        Console.WriteLine("Must define " + DaikonEnvVar + " environment variable");
+        Environment.Exit(1);
+      }
+      return daikonDir;
+    }
+
+    /// <summary>
+    /// Generate the comparability information for the given assembly.
+    /// </summary>
+    private static AssemblySummary GenerateComparability(FrontEndArgs frontEndArgs, TypeManager typeManager, 
+      IMetadataHost host, IModule module, PdbReader pdbReader, ref Assembly mutable)
+    {
+      AssemblySummary comparabilityManager = null;
+      IAssembly assembly = module as IAssembly;
+      mutable = MetadataCopier.DeepCopy(host, assembly);
+      typeManager.SetAssemblyIdentity(UnitHelper.GetAssemblyIdentity(mutable));
+
+      if (frontEndArgs.StaticComparability || frontEndArgs.GenerateComparability)
+      {
+        if (frontEndArgs.ComparabilityFile != null)
+        {
+          using (var cmp = File.Open(frontEndArgs.ComparabilityFile, FileMode.Open))
+          {
+            comparabilityManager = (AssemblySummary)new BinaryFormatter().Deserialize(cmp);
+          }
+        }
+        else
+        {
+          if (frontEndArgs.VerboseMode)
+          {
+            Console.WriteLine("Generating Comparability Information");
+          }
+
+          Assembly decompiled = Decompiler.GetCodeModelFromMetadataModel(typeManager.Host, mutable,
+            pdbReader, DecompilerOptions.AnonymousDelegates | DecompilerOptions.Iterators);
+          comparabilityManager = AssemblySummary.MakeSummary(decompiled, typeManager, pdbReader);
+
+          if (frontEndArgs.VerboseMode)
+          {
+            Console.WriteLine("Finished Generating Comparability Information");
+          }
+
+          using (var cmp = File.Open(frontEndArgs.AssemblyPath + FrontEndArgs.ComparabilityFileExtension, FileMode.Create))
+          {
+            new BinaryFormatter().Serialize(cmp, comparabilityManager);
+          }
+        }
+      }
+      return comparabilityManager;
+    }
+
+    /// <summary>
+    /// Load the PDB reader and pdb file for the source program.
+    /// </summary>
+    private static void LoadPdbReaderAndFile(FrontEndArgs frontEndArgs, TypeManager typeManager,
+      IModule module, out PdbReader pdbReader, out string pdbFile)
+    {
+      pdbReader = null;
+      pdbFile = Path.ChangeExtension(module.Location, "pdb");
+      try
+      {
+        using (var pdbStream = File.OpenRead(pdbFile))
+        {
+          pdbReader = new PdbReader(pdbStream, typeManager.Host);
+        }
+      }
+      catch (System.IO.IOException)
+      {
+        if (frontEndArgs.StaticComparability)
+        {
+          throw new InvalidOperationException("Error loading PDB file for '" +
+            module.Name.Value + "' (required for static comparability analysis)");
+        }
+        else
+        {
+          // TODO(#25): Figure out how what happens if we can't load the PDB file.
+          // It seems to be non-fatal, so print the error and continue.
+          Console.Error.WriteLine("WARNING: Could not load the PDB file for '" + module.Name.Value + "'");
+        }
+      }
+    }
+  }
+}
---
+++ DotNetFrontEndTests/VisualBasic/VisualBasic.vb
@@ -0,0 +1,89 @@
+﻿Module QuickSorter
+    Sub qsort(ByRef a() As Object)
+        Dim i
+        Dim ii
+        For i = 0 To a.Length() - 1
+            ii = New System.Random().Next(0, a.Length() - 1)
+            If i <> ii Then
+                swap(a(i), a(ii))
+            End If
+        Next
+
+        quicksort(a, 0, a.Length())
+    End Sub
+
+    Sub print(ByRef a() As Object)
+        Dim i
+        For i = 0 To a.Length() - 1
+            System.Console.Write("  " & a(i))
+        Next
+        System.Console.WriteLine()
+    End Sub
+
+    Sub main()
+        Dim towns() As Object = {"Paris", "London", "Stockholm", "Berlin", "Oslo", "Rome", _
+        "Madrid", "Tallinn", "Amsterdam", "Dublin"}
+
+        System.Console.Write("towns before qsort: ")
+        print(towns)
+
+        qsort(towns)
+
+        System.Console.Write("towns after qsort: ")
+        print(towns)
+
+        Dim numbers() As Object = {5, 7, 5, 2, 8, 4, 6, 2, 3, 5, 1, 8, 5, 7, 3}
+
+        System.Console.Write("numbers before qsort: ")
+        print(numbers)
+
+        qsort(numbers)
+
+        System.Console.Write("numbers after qsort: ")
+        print(numbers)
+    End Sub
+
+    Sub quicksort(ByRef a() As Object, ByVal left As Integer, ByVal right As Integer)
+        Dim pivot As Integer
+        If right - left > 1 Then
+            pivot = getpivot(a, left, right)
+            pivot = partition(a, left, right, pivot)
+            quicksort(a, left, pivot)
+            quicksort(a, pivot + 1, right)
+        End If
+    End Sub
+
+    Function getpivot(ByRef a() As Object, ByVal left As Integer, ByVal right As Integer)
+        Return New System.Random().Next(left, right - 1)
+    End Function
+
+    Function partition(ByRef a() As Object, ByVal left As Integer, _
+       ByVal right As Integer, ByRef pivot As Integer)
+        Dim i
+        Dim piv
+        Dim store
+
+        piv = a(pivot)
+
+        swap(a(right - 1), a(pivot))
+
+        store = left
+        For i = left To right - 2
+            If a(i) <= piv Then
+                swap(a(store), a(i))
+                store = store + 1
+            End If
+        Next
+        swap(a(right - 1), a(store))
+
+        Return store
+    End Function
+
+    Sub swap(ByRef v1, ByRef v2)
+        Dim tmp
+        tmp = v1
+        v1 = v2
+        v2 = tmp
+    End Sub
+
+End Module
